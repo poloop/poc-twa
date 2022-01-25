28ee9983496e7e0356338f8fdee9246f50f58515
Paul Long <poloop@gmail.com>
2022-01-25 12:21:29 +0100

    first commit

---
+++ app/src/main/java/app/netlify/optimistic_bohr_fc5c22/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package app.netlify.optimistic_bohr_fc5c22.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
